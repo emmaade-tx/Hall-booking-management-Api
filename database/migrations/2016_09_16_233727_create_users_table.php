c5ea920f2bb9763d4fa2c6acad25d77b5e1560a4
Emmanuel Ade <emmade.tx@gmail.com>
2016-09-17 10:28:39 +0100

    fix(migrations): create users and halss migration table

---
+++ database/migrations/2016_09_16_233727_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('username')->unique();
+            $table->string('email')->nullable();
+            $table->string('password');
+            $table->integer('role_id')->default(1);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('avatar')->nullable();
+            $table->string('provider')->nullable();
+            $table->integer('provider_id')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::drop('users');
+    }
+}
