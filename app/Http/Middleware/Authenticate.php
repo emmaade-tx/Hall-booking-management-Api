1509cf9897660d25327de09dec169950c64e7544
Emmanuel Ade <emmade.tx@gmail.com>
2016-11-12 13:56:49 +0100

    fix travis and DRY issue

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\User;
 use Closure;
+use App\Http\StatusCode;
+use App\Http\Middleware\Constant;
 use Illuminate\Contracts\Auth\Factory as Auth;
 
 class Authenticate
@@ -13,6 +16,7 @@ class Authenticate
      * @var \Illuminate\Contracts\Auth\Factory
      */
     protected $auth;
+    protected $statusCode;
 
     /**
      * Create a new middleware instance.
@@ -20,9 +24,10 @@ class Authenticate
      * @param  \Illuminate\Contracts\Auth\Factory  $auth
      * @return void
      */
-    public function __construct(Auth $auth)
+    public function __construct(Auth $auth, StatusCode $statusCode)
     {
         $this->auth = $auth;
+        $this->statusCode = $statusCode;
     }
 
     /**
@@ -35,10 +40,33 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
-        }
+        // if ($this->auth->guard($guard)->guest()) {
+        //     return response()->json(['message' => 'Please login'], $this->statusCode->unauthorised);
+        // }
 
-        return $this->readHandle($request, $next);
+        return $this->checkUser($request, $next);
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function checkUser($request, Closure $next)
+    {
+        $token = $request->query('token');
+            
+            if (!empty($token)) {
+                $appToken = User::where('api_token', '=', $token)
+                    ->first();
+
+                if (is_null($appToken)) {
+                    return response()->json(['message' => 'User unauthorized due to invalid token'], $this->statusCode->unauthorised);
+                }
+
+                return $this->approveUser($request, $appToken, $next);
+            }
     }
 }
--- app/Http/Middleware/RegularMiddleware.php
+++ app/Http/Middleware/RegularMiddleware.php
@@ -6,19 +6,14 @@ use Closure;
 use App\User;
 use App\Http\StatusCode;
 use App\Http\Middleware\Constant;
-use Illuminate\Contracts\Auth\Factory as Auth;
 
 class RegularMiddleware extends Authenticate
 {
     protected $statusCode;
-    protected $handle;
-    protected $auth;
 
-    public function __construct(StatusCode $statusCode, Handle $handle, Auth $auth)
+    public function __construct(StatusCode $statusCode)
     {
         $this->statusCode = $statusCode;
-        $this->handle = $handle;
-        $this->auth = $auth;
     }
 
     /**
@@ -30,21 +25,10 @@ class RegularMiddleware extends Authenticate
      */
     public function readHandle($request, Closure $next)
     {
-        $token = $request->query('token');
-            
-            if (!empty($token)) {
-                $appToken = User::where('api_token', '=', $token)
-                    ->first();
-
-                if (is_null($appToken)) {
-                    return response()->json(['message' => 'User unauthorized due to invalid token'], $this->statusCode->unauthorised);
-                }
-
-                return $this->checkUser($request, $appToken, $next);
-            }
+        return $this->handle($request, $next);
     }
 
-    public function checkUser($request, $appToken, $next)
+    public function approveUser($request, $appToken, $next)
     {
         if ($appToken->role_id >= Constant::SUPER_ADMIN_USER) {
                 return $next($request);
